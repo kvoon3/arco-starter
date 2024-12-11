c3f25f03e7d9c4ce29c754de9086c62557d099c3
kvoon <a1176079417@gmail.com>
2024-12-11 15:59:20 +0800

    chore: update

--- src/pages/message.vue
+++ src/pages/message.vue
@@ -7,11 +7,11 @@ import { weilaFetch } from '~/api/instances/fetcher'
 
 definePage({
   meta: {
-    menu: {
-      label: 'menu.message',
-      icon: 'icon-user',
-      order: 0,
-    },
+    // menu: {
+    //   label: 'menu.message',
+    //   icon: 'icon-user',
+    //   order: 0,
+    // },
   },
 })
 
@@ -84,13 +84,11 @@ const { data: groups } = useQuery<GroupModel[]>({
         </ListboxContent>
       </ListboxRoot> -->
       <a-empty v-if="!groups || !groups.length" absolute position-center />
-      <RouterLink
-        v-for="group in groups" :key="group.id" :to="`/message/${group.id}-${group.name}`"
+      <RouterLink v-for="group in groups" :key="group.id" :to="`/message/${group.id}-${group.name}`"
         class="flex items-center gap-2 list-btn" :class="route.params.group_id && group.id === Number(route.params.group_id)
           ? 'bg-gray-100 dark:bg-dark-2'
           : ''
-        "
-      >
+          ">
         <UseImage :src="group.avatar" class="mr-4 h-12 w-12 rounded-full" alt="Group Avatar">
           <template #loading>
             <div class="mr-4 h-12 w-12 animate-pulse rounded-full bg-gray-200" />
