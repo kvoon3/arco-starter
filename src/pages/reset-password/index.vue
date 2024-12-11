6e468415e26d9da47199b604750a97ec10265761
kvoon <a1176079417@gmail.com>
2024-12-11 15:56:46 +0800

    fix: i18n

--- src/pages/reset-password/index.vue
+++ src/pages/reset-password/index.vue
@@ -41,7 +41,7 @@ const { mutate: submit, isPending: isSubmiting } = useMutation({
     })
   },
   onSuccess() {
-    Message.success('Success')
+    Message.success(t('message.success'))
   },
   onError(error) {
     Message.error(error.message || 'Request Error')
@@ -58,48 +58,38 @@ const { mutate: submit, isPending: isSubmiting } = useMutation({
     </h2>
 
     <a-form :model="form" layout="vertical" @submit="() => submit(form)">
-      <a-form-item
-        :label="t('phone-number')" field="phone"
+      <a-form-item :label="t('phone-number')" field="phone"
         :rules="[{ required: true, message: t('binding-phone-form.err-msg.phone-number') }]"
-        :validate-trigger="['blur', 'change']"
-      >
+        :validate-trigger="['blur', 'change']">
         <a-input v-model="form.phone" />
       </a-form-item>
 
-      <a-form-item
-        :label="t('verify-image-code')" field="verify_img_code"
+      <a-form-item :label="t('verify-image-code')" field="verify_img_code"
         :rules="[{ required: true, message: t('binding-phone-form.err-msg.verify-image-code') }]"
-        :validate-trigger="['blur', 'change']"
-      >
+        :validate-trigger="['blur', 'change']">
         <div class="flex items-center">
           <a-input v-model="form.verify_img_code" class="mr-2 flex-grow" />
           <VerifyImg ref="verifyImg" class="flex-shrink-0" />
         </div>
       </a-form-item>
 
-      <a-form-item
-        :label="t('sms-code')" field="verify_code"
+      <a-form-item :label="t('sms-code')" field="verify_code"
         :rules="[{ required: true, message: t('binding-phone-form.err-msg.verify-code') }]"
-        :validate-trigger="['blur', 'change']"
-      >
+        :validate-trigger="['blur', 'change']">
         <div class="flex items-center">
           <a-input v-model="form.verify_code" class="mr-2 flex-grow" />
-          <SendSmsButton
-            classes="flex-shrink-0" :opts="{
-              phone: form.phone,
-              verify_code: form.verify_img_code,
-              country_code: '86',
-              sms_type: 'reset-password',
-            }" @error="() => verifyImg?.refetch()"
-          />
+          <SendSmsButton classes="flex-shrink-0" :opts="{
+            phone: form.phone,
+            verify_code: form.verify_img_code,
+            country_code: '86',
+            sms_type: 'reset-password',
+          }" @error="() => verifyImg?.refetch()" />
         </div>
       </a-form-item>
 
-      <a-form-item
-        :label="t('password')" field="password"
+      <a-form-item :label="t('password')" field="password"
         :rules="[{ required: true, message: t('binding-phone-form.err-msg.password') }]"
-        :validate-trigger="['blur', 'change']"
-      >
+        :validate-trigger="['blur', 'change']">
         <a-input-password v-model="form.password" />
       </a-form-item>
 
